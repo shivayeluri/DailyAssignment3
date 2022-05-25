212efa3fd2471b239b382f3ea903fa2082d6fc51
shivakumar.y <shivakumar.y@perfios.com>
2022-05-25 18:21:20 +0530

    Added : to implement main class

---
+++ src/assignment3/Main.java
@@ -0,0 +1,13 @@
+package assignment3;
+
+import assignment3.logic.*;
+
+public class Main {
+    public static void main(String[] args) {
+//        new CharacterCheck().vowelOrConsonant(args);
+//        new LargestNumber().amongThreeNumbers(args);
+//        new NumberCheck().evenOrOdd(args);
+        new QuotientRemainder().calculateQuotientRemainder(args);
+//        new Swapping().swapTwoNumbers(args);
+    }
+}
